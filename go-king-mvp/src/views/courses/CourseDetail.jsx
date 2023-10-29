6a9b0b2d37a8b0bd2c2aa1f82cb738eedca8e26b
Afitaganda <creatifypudding@gmail.com>
2023-10-29 20:11:00 +0700

    update course detail

--- go-king-mvp/src/views/courses/CourseDetail.jsx
+++ go-king-mvp/src/views/courses/CourseDetail.jsx
@@ -1,48 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
 
 const CourseDetail = () => {
-  const [courseData, setCourseData] = useState(null);
+  const location = useLocation()
+  const navigate = useNavigate()
 
-  useEffect(() => {
-    const fetchCourseData = async () => {
-      try {
-        const response = await axios.get('URL_MOCKAPI');
-        setCourseData(response.data);
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-      }
-    };
+  const courseData = location.state.value
+  const whatsappLink = `https://wa.me/${courseData.contact.trim()}`
 
-    fetchCourseData();
-  }, []);
+  console.log(whatsappLink);
 
   const handleBack = () => {
-    history.push('/courses');
+    navigate('/course-search');
   };
 
   return (
-    <div className="container mx-auto my-8">
-      {courseData ? (
-        <div>
-          <h2 className="text-3xl font-bold mb-4">{courseData.title}</h2>
-          <p>Type: {courseData.type}</p>
-          <p>Duration: {courseData.duration}</p>
-          <p>Accent: {courseData.accent}</p>
-          <p>Facility: {courseData.facility}</p>
-          <p>Description: {courseData.description}</p>
-          <p>Contact: {courseData.contact}</p>
+    <div className="container mx-auto my-8 bg-emerald-100 h-screen text-stone-800">
+      <Navbar />
+      <div className="max-w-screen-xl m-auto p-8 flex flex-col items-center ">
+        <h2 className="text-3xl font-bold mb-4">{courseData.title}</h2>
+        <p>Type: {courseData.type}</p>
+        <p>Duration: {courseData.duration}</p>
+        <p>Accent: {courseData.accent}</p>
+        <p>Facility: {courseData.facility}</p>
+        <p>Description: {courseData.description}</p>
+        <button >
+          <a href={whatsappLink}
+            className="text-slate-50">WhatsApp</a>
+        </button>
 
-          <button
-            onClick={handleBack}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Back to Courses
-          </button>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+        <button
+          onClick={handleBack}
+          className="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 mt-4 w-2/5 rounded focus:outline-none focus:shadow-outline"
+        >
+          Back to Courses
+        </button>
+      </div>
     </div>
   );
 };
